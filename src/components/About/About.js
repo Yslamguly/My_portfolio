96f042a956f0506c1e9038e81fd1cc4f449f3c96
Yslamguly <islamguly28@gmail.com>
2022-08-04 12:14:03 +0200

    implemented About.js

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Aboout.css'
+const About = () =>{
+    return(
+        <section className={'angled-background'} id={'about'} >
+            <div className={'vh-100 flex flex-column justify-center center fw7 tc pl3 pr3'} style={{fontSize:'1.8em'}}>
+                <h2 className={''}>About me</h2>
+            </div>
+        </section>
+    )
+}
+
+export default About
