2651c5fc125b1e4ae9a9879122fae0b9b8bec14a
Yslamguly <islamguly28@gmail.com>
2022-08-04 12:13:41 +0200

    implemented NavBar.js

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../About/Aboout.css'
+
+
+const NavBar = () =>{
+    return(
+        <nav className="dt w-100 border-box pa3 ph5-ns angled-background">
+            <div className="dtc v-mid w-75 tr">
+                <a className="link dim white f6 f5-ns dib mr3 mr4-ns" href="#" title="CV">CV</a>
+                <a className="link dim white f6 f5-ns dib mr3 mr4-ns" href="https://www.linkedin.com/in/yslamguly-pirgulyyev-60a373225/" title="LinkedIn">LinkedIn</a>
+                <a className="link dim white f6 f5-ns dib" href="https://github.com/Yslamguly" title="GitHub">GitHub</a>
+            </div>
+        </nav>
+    )
+}
+export default NavBar
