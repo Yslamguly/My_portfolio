47839af717c614271e12ed506fd9e3cf7e098757
Yslamguly <islamguly28@gmail.com>
2022-08-06 11:14:57 +0200

    added media queries for navbar background

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import './Aboout.css'
 const About = () =>{
     return(
-        <section className={'angled-background'} id={'about'} >
+        <section className={'nav-background'} id={'about'} >
             <div className={'vh-100 flex flex-column justify-center center fw7 tc pl3 pr3'} style={{fontSize:'1.8em'}}>
                 <h2 className={''}>About me</h2>
             </div>
