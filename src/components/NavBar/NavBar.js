5bb8bdfb436be0b972bd330fc44a126f13824b07
Yslamguly <islamguly28@gmail.com>
2022-08-07 14:14:15 +0200

    changed the background color of the nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,14 @@
 import React from "react";
-import '../About/Aboout.css'
+import './NavBar.css'
+import MyResume from '../../assets/Yslamguly-Resume Junior.pdf'
 
-
-const NavBar = () =>{
-    return(
-        <nav className="dt w-100 border-box pa3 ph5-ns angled-background">
+const NavBar = () => {
+    return (
+        <nav className="dt w-100 border-box pa3 ph5-ns nav-background z-1 fixed">
             <div className="dtc v-mid w-75 tr">
-                <a className="link dim white f6 f5-ns dib mr3 mr4-ns" href="#" title="CV">CV</a>
-                <a className="link dim white f6 f5-ns dib mr3 mr4-ns" href="https://www.linkedin.com/in/yslamguly-pirgulyyev-60a373225/" title="LinkedIn">LinkedIn</a>
+                <a className="link dim white f6 f5-ns dib mr3 mr4-ns" href={MyResume} target="_blank" rel="noreferrer" title="CV">View Resume</a>
+                <a className="link dim white f6 f5-ns dib mr3 mr4-ns"
+                   href="https://www.linkedin.com/in/yslamguly-pirgulyyev-60a373225/" title="LinkedIn">LinkedIn</a>
                 <a className="link dim white f6 f5-ns dib" href="https://github.com/Yslamguly" title="GitHub">GitHub</a>
             </div>
         </nav>
