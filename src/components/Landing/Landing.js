ae623dd960636d45bbd5b9c91b10282a9cffbb4a
Yslamguly <islamguly28@gmail.com>
2022-08-13 10:55:50 +0200

    animated the landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,21 +1,25 @@
 import React from "react";
-import '../About/Aboout.css'
 import './Landing.css'
-import MyImage from '../Images/IMG_4322.jpg'
+import MyImage from '../../Images/IMG_4322.jpg'
 
 const Landing = () => {
     return (
-        <section className={''}>
-            <div className={'container fw7   pl3 pr3 font'}>
-                <h1 className={'tl ml3 pr6'}>Hi, my name is<span className={'light-green'}>Yslamguly</span>
-                    <br/>I'm a CS student <br/>
-                    <span style={{fontSize: '0.5em'}}>
-                            <a className={'light-green tc mt5 h-100 w-25 link hover-bg-light-green hover-white'}
-                               href="#about">About me
-                            </a>
+        <section>
+            <div className={'container fw7 pt5  pl3 pr3 font'}>
+                <h1 className={'tl ml3 pr6 animate__animated animate__slideInLeft animate__slow'}>Hi, my name is <span className={'light-green'}>Yslamguly</span>
+                    <br/>I'm a CS student. <br/>
+                    <span className={'about-me-link'}>
+                        <a className={'light-green ba bw1 pr1 pl1 tc mt3 ml3 h-100 dib link ' +
+                        'hover-bg-light-green hover-white ' +
+                        'animate__animated ' +
+                        'animate__slideInLeft ' +
+                        'animate__delay-1s'}
+                           href="#about">About me
+                        </a>
                     </span>
                 </h1>
-                <img src={MyImage} className={'mr2 br4 grow shadow-5'} style={{height: '60%'}} alt="night sky over water"/>
+                <img src={MyImage} className={'mr2 br4 grow shadow-5'}
+                     alt="me"/>
             </div>
         </section>
     )
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'tachyons';
+import 'animate.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NavBar from "./components/NavBar/NavBar";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <NavBar/>
+      <App />
   </React.StrictMode>
 );
 
