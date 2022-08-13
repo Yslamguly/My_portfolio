953bb797d49363345ff623d5dccb9ad3494095e5
Yslamguly <islamguly28@gmail.com>
2022-08-13 11:01:21 +0200

    built the structure and styled "about me" section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,43 @@
 import React from "react";
 import './Aboout.css'
-const About = () =>{
-    return(
-        <section className={'nav-background'} id={'about'} >
-            <div className={'vh-100 flex flex-column justify-center center fw7 tc pl3 pr3'} style={{fontSize:'1.8em'}}>
-                <h2 className={''}>About me</h2>
+import MyImage from "../../Images/IMG_5216.jpg";
+
+
+const About = () => {
+    return (
+        <section id={'about'}>
+            <div className="mw9 center ph3-ns">
+                <div className="cf ph2-ns">
+                    <div className="fl w-100 pa2">
+                        <div className=" pv4"><h1 className={'section-title'} style={{marginTop: '2rem'}}>About me</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className="mw9 center ph3-ns">
+                    <div className="cf ph2-ns">
+                        <div className="fl w-100 w-50-ns pa2">
+                            <div className=" pv4"><img src={MyImage} className={'br4 grow shadow-5 my-image'}
+                                                       alt="me"/></div>
+                        </div>
+                        <div className="fl w-100 w-50-ns pa2">
+                            <div className="pt5-ns pt3-m pt0-m tl-ns tc lh-copy ">
+                                <p className={'pt3'}>Ewrklnfkejrbgkrebgekrbge kjrbgekrbgekrg erkgnerjkgnerjkg
+                                    erkgjnerjgknerg erkgnjerkjgre g er ge
+                                    efbehrfberhjgfbehjgerbger gejhbgejhgbrejhgberjgberjger grjhbergjergreger</p>
+                                <p className={'pt3'}>Rejnfgejkrg ekjrgberhkg gkerbgekrgnrejkq gkgjqnrgj rkegjnregk
+                                    ergkejnrjge
+                                    rkgkregerng
+                                    ergkejrgn
+                                    ergjkbekgbegbe jgkjebgekgjebgkegb ekrgberkgerbg jkerbgerkg ejkrgberkjgerg
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
+
 }
 
 export default About
