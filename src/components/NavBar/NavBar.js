11ad1271b41440e7c0f257718dd9e3e53a2ffd2f
Yslamguly <islamguly28@gmail.com>
2023-02-01 15:19:31 +0100

    update build for gh-pages

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './NavBar.css'
-import MyResume from '../../assets/Yslamguly Pirgulyyev'
+import MyResume from '../../assets/Yslamguly Pirgulyyev.pdf'
 
 const NavBar = () => {
     return (
